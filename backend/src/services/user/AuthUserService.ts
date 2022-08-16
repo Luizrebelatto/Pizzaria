e193994c87c13330db3cc89b824134eae119642a
luiz <lbrebelatto@gmail.com>
2022-08-15 21:20:45 -0300

    feat: user authentication

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,28 @@
+import prismaClient from "../../prisma";
+import { compare } from 'bcryptjs';
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if (!user) throw new Error("user/password incorrect");
+
+    const passwordMatch = await compare(password, user.password);
+
+    if(!passwordMatch) throw new Error("user/password incorrect");
+
+    return {ok: true}
+  }
+}
+
+export { AuthUserService }
