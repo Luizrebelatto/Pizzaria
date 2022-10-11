1f0472e0aade299c1217995f0a74b7bfe3c210c3
luiz <lbrebelatto@gmail.com>
2022-10-11 12:57:28 -0300

    doc: swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
+import swaggerUi from 'swagger-ui-express'
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import path from 'path';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
